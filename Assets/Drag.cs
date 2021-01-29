817c656bb8450ad4abfc9eee84d114839039b704
Robel Getnet <robelgetnetrg@gmail.com>
2021-01-29 23:29:19 +0300

    Initial commit

---
+++ Assets/Drag.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private Vector2 _origin;
+
+    private Vector2 _distance;
+    
+    private Vector2 _lastPosition;
+
+    public Rotor rotor;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _origin = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _distance = eventData.position - _lastPosition;
+
+        _lastPosition = eventData.position;
+        
+        //rotor.Rotate(_distance);
+    }
+}
---
+++ Assets/Rotor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotor : MonoBehaviour
+{
+    public float sensitivity;
+    
+    public Rigidbody rBody;
+
+    private Vector2 _inputVector;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        _inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
+        Rotate(_inputVector);
+    }
+
+    public void Rotate(Vector2 inputVector)
+    {
+        inputVector *= sensitivity;
+
+        if (inputVector == Vector2.zero)
+        {
+            rBody.Sleep();
+        }
+        
+        rBody.AddTorque(inputVector.y, 0, - inputVector.x);
+    }
+
+    private void FixedUpdate()
+    {
+//        Rotate(_inputVector);
+    }
+}
